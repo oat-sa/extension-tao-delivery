d75043251584b421c97ad0b104bc18a460482008
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-10 15:03:22 +0000

    manifest update
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoDelivery/trunk@1771 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -1,3 +1,17 @@
 <?php
-
+	return array(
+		'name' => 'TAO Delivery',
+		'description' => 'TAO http://www.tao.lu',
+		'additional' => array(
+			'version' => '1.0',
+			'author' => 'CRP Henry Tudor',
+			'dependances' => array(),
+			'install' => array( 
+				'sql' => dirname(__FILE__). '/model/ontology/TAODelivery.sql',
+				'php' => dirname(__FILE__). '/install/install.php'
+			),
+			'configFile' => dirname(__FILE__). '/includes/common.php'	
+			
+		)
+	);
 ?>
