78975222d4d9eabde137f329b2d992cff2cfa0c7
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-12-17 15:03:39 +0000

    inetgrate user auth
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoDelivery/trunk@1827 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/common.php
+++ includes/common.php
@@ -1,16 +1,15 @@
 <?php
-
-
-
-
-
+/*
+ * 
+ * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
+ * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
+ * 
+ */
+session_start();
 require_once 	dirname(__FILE__). "/config.php";
 require_once 	dirname(__FILE__). "/constants.php";
 
-
-
-
 set_include_path(get_include_path() . PATH_SEPARATOR . GENERIS_BASE_PATH.'/..');
 
-core_control_FrontController::connect(API_LOGIN, API_PASS, API_MODULE);
+include_once 'tao/includes/prepend.php';
 ?>
