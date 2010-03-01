2c8a9d273c21331f54396f2df1a329870b423c0a
ydjaghloul <ydjaghloul@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-03-01 14:21:31 +0000

    ###Younes Djaghloul, CRP Henri Tudor, Luxembourg### LOG export to TAO based on cURL
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoDelivery/trunk@2272 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/deliveryServer/resultServer/uploadResultServer.php
+++ views/deliveryServer/resultServer/uploadResultServer.php
@@ -3,11 +3,11 @@
 called from tao_result_wsdl.php
 */
 /**
-* Implements web service for upload of test results
-* @package plugins.uploadresultserver
-* @author Plichart Patrick <patrick.plichart@tudor.lu>
-* @version 1.1
-*/
+ * Implements web service for upload of test results
+ * @package plugins.uploadresultserver
+ * @author Plichart Patrick <patrick.plichart@tudor.lu>
+ * @version 1.1
+ */
 
 //split xml to an array containing all results
 include("split.php");
@@ -22,12 +22,12 @@ include("cacheResults.php");
 include_once("nusoap.php");
 
 /**
-* implements server receiving informations about results
-* For A Client Application
-* @author patrick
-* @access public
-* @package taoplugins
-*/
+ * implements server receiving informations about results
+ * For A Client Application
+ * @author patrick
+ * @access public
+ * @package taoplugins
+ */
 
 $server = new nusoap_server();
 $server->debug_flag=true;
@@ -37,47 +37,93 @@ error_reporting(0);
 /*
 *sends back the list of missing packets to the client
 */
-function isFullyOk($IDresult,$numberElts){
-	return isFullyOkResult($IDresult[0],$numberElts[0]);
+function isFullyOk($IDresult,$numberElts) {
+    return isFullyOkResult($IDresult[0],$numberElts[0]);
 }
 
 /*
 * deal with one packet received from the client, the resutl is identified with IDresult, $length gives the number of packets to be received for this result, $seq defines the order number for this apcket (starts with the 0th packet)
 */
-function setResult($xml,$IDresult,$seq,$length){
+function setResult($xml,$IDresult,$seq,$length) {
 
-$return = getFullXml($xml[0], $IDresult[0], $seq[0], $length[0]);
+    $return = getFullXml($xml[0], $IDresult[0], $seq[0], $length[0]);
 
 
-if (!($return)) {return "OK";die();} 
-else {$listXmls = splitXML($return);}
+    if (!($return)) {
+        return "OK";
+        die();
+    }
+    else {
+        $listXmls = splitXML($return);
+    }
 
-$xmlString="";
-foreach ($listXmls as $key=> $val){
-	$xml =array($val);
-	$today = date("F j, Y, g i a").time().rand(0,256);
+    $xmlString="";
+    foreach ($listXmls as $key=> $val) {
+        $xml =array($val);
+        $today = date("F j, Y, g i a").time().rand(0,256);
 
-	$xmlFile="Result ".$today." ".$IDresult[0].".xml";
-	//Hack, an old bug in the xml of item caused problems with some items created by MRE
-	$xml[0]=str_replace("http://mod1.tao.lu/middleware/MoniqueReichertItems.rdfhttp","http",$xml[0]);
+        $xmlFile="Result ".$today." ".$IDresult[0].".xml";
+        //Hack, an old bug in the xml of item caused problems with some items created by MRE
+        $xml[0]=str_replace("http://mod1.tao.lu/middleware/MoniqueReichertItems.rdfhttp","http",$xml[0]);
 
-	$fp = fopen("./received/$xmlFile", "wb");
-	fwrite($fp,$xml[0]);
-	fclose($fp);
+        $fp = fopen("./received/$xmlFile", "wb");
+        fwrite($fp,$xml[0]);
+        fclose($fp);
 
-	$xmlString.=$xml[0];
-}
+        $xmlString.=$xml[0];
+    }
 
 //send the link to the document to the taoResults extension
-$xmlPath=dirname(__FILE__)."/received/$xmlFile";
-$location='http://'.$_SERVER['HTTP_HOST']."/taoResults/models/ext/utrv1/classes/importLogToGenerisResult.php?resultxml=".urlencode($xmlPath);
+    $xmlPath=dirname(__FILE__)."/received/$xmlFile";
+    $location='http://'.$_SERVER['HTTP_HOST']."/taoResults/models/ext/utrv1/classes/importLogToGenerisResult.php";//?resultxml=".urlencode($xmlPath);
+    /*//send with curl
+    $url = $location;
 
-//Using the included scripts from the Result extension to import result data in Result Ontologies
+    $ch = curl_init();
+    //set options
+    curl_setopt($ch,CURLOPT_URL,$url);
+
+    //curl_setopt($ch,CURLOPT_POST,1);
+    //curl_setopt($ch,CURLOPT_POSTFIELDS,$postFieldes);
+    curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);// allow redirect
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); // return into a variable
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60); // 
+    //close the cURL connexion
+
+    $result = curl_exec($ch);*/
+
+    //
+
+    //send th XML itself
+    $url = $location;
+//send path of the file and the content too.
+    $postFileds = array ();
+
+    $postFileds['pathLogFile'] = urlencode($xmlPath);
+    $postFileds["contentLogFile"] = urlencode($xmlString);
+    $ch = curl_init();
+    //set options
+    curl_setopt($ch,CURLOPT_URL,$url);
+
+    curl_setopt($ch,CURLOPT_POST,1);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$postFileds);//'pathLogFile='.urlencode($xmlPath));
+
+    curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);// allow redirects
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); // return into a variable
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60); // times out after 4s
+    //close the cURL connexion
+
+    $result = curl_exec($ch);
+
+
+// Using the included scripts from the Result extension to import result data in Result Ontologies
 // $resultDOM=new DomDocument();
 // $resultDOM->loadXML($xmlString);
 // $importResult=new importLog($resultDOM);
 
-return "OK";
+    return "OK";
 }
 
 
