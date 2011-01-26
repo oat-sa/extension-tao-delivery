0c0578eb29a17904553563d193821018daf4900f
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-26 14:37:45 +0000

    * Update extensions manifests to include model dependancy
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoDelivery/trunk@4352 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -1,29 +1,26 @@
 <?php
-	return array(
-		'name' => 'TAO Delivery',
-		'description' => 'TAO http://www.tao.lu',
-		'additional' => array(
-			'version' => '1.2',
-			'author' => 'CRP Henri Tudor',
-			'dependances' => array(),
-			'install' => array( 
-				'sql' => dirname(__FILE__). '/model/ontology/TAODelivery.sql',
-				'php' => dirname(__FILE__). '/install/install.php'
-			),
-			
-			'model' => array( 
-							'http://www.tao.lu/Ontologies/TAODelivery.rdf',
-							'http://www.tao.lu/Ontologies/TAOGroup.rdf',
-							'http://www.tao.lu/Ontologies/TAOSubject.rdf',
-							'http://www.tao.lu/Ontologies/TAOTest.rdf'
-			),
-		
-			'classLoaderPackages' => array( 
-				dirname(__FILE__).'/actions/',
-				dirname(__FILE__).'/helpers/'
+/*
+ * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
+ * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
+ * 
+ */
+return array(
+	'name' => 'taoDelivery',
+	'description' => 'TAO http://www.tao.lu',
+	'additional' => array(
+		'version' => '2.0',
+		'author' => 'CRP Henri Tudor',
+		'dependances' => array('wfEngine'),
+		'models' => 'http://www.tao.lu/Ontologies/TAODelivery.rdf',
+		'install' => array( 
+			'php' => dirname(__FILE__). '/install/install.php',
+			'rdf' => dirname(__FILE__). '/models/ontology/taodelivery.rdf'
+		),
+		'classLoaderPackages' => array( 
+			dirname(__FILE__).'/actions/',
+			dirname(__FILE__).'/helpers/'
 
-			 )	
-			
-		)
-	);
+		 )	
+	)
+);
 ?>
