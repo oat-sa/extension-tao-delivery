70b90da9ce0366f60a747316f63045b85c5463ec
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-03-29 14:38:28 +0000

    basic preview and next button added
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoDelivery/trunk@5171 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/class.WebService.php
+++ actions/class.WebService.php
@@ -6,7 +6,13 @@ class taoDelivery_actions_WebService extends tao_actions_CommonModule{
 		$width = $this->hasRequestParameter('width')?$this->getRequestParameter('width'):'100%';
 		$height = $this->hasRequestParameter('height')?$this->getRequestParameter('height'):'100%';
 		$url = urldecode($this->getRequestParameter('url'));
-		echo '<iframe src ="'.$url.'" width="'.$width.'" height="'.$height.'"/>';
+		$content = '<iframe id="webWerviceFrame" src ="'.$url.'" width="'.$width.'" height="'.$height.'"/>';
+		$control = '<form>
+				<input type="button" VALUE="&lt; &lt; &nbsp; Back &nbsp;"  onClick="history.back()"> 
+				<input type="button" VALUE="Forward &nbsp; &gt; &gt;"  onClick="history.forward()"> 
+			</form>';
+			
+		echo $control.$content;
 	}
 }
 ?>
