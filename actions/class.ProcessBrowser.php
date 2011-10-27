dd3f67524806746f217c0b5911645656af952585
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-10-27 15:19:35 +0000

    - implement activity redireting after login - added auto redirecting if the activity defined in link is outdated
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoDelivery/trunk@7199 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/class.ProcessBrowser.php
+++ actions/class.ProcessBrowser.php
@@ -4,6 +4,11 @@ error_reporting(E_ALL);
 
 class taoDelivery_actions_ProcessBrowser extends wfEngine_actions_ProcessBrowser{
 	
+	public function __construct(){
+		parent::__construct();
+		$this->autoRedirecting = false;
+	}
+	
 	protected function redirectToMain(){
 		Session::removeAttribute("processUri");
 		$this->redirect(tao_helpers_Uri::url('index', 'DeliveryServer'));
@@ -12,6 +17,9 @@ class taoDelivery_actions_ProcessBrowser extends wfEngine_actions_ProcessBrowser
 	public function loading(){
 		$this->setView('itemLoading.tpl');
 	}
-
+	
+	protected function autoredirectToIndex(){
+		$this->redirectToIndex();
+	}
 }
 ?>
