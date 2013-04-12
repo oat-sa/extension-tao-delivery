cb97dbbf64674cccf234265ea43fb9e76e4f27d4
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-04-12 14:48:16 +0000

    IETF language Tags !!!
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoDelivery/trunk@14514 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/EN/messages_po.js
+++
@@ -1,4 +0,0 @@
-/* auto generated content */
-/* lang: EN */
-var langCode = 'EN';
-var i18n_tr = {"The Result Server Management section allows to add, edit and assign Delivery Result Server to Deliveries.":"The Result Server Management section allows to add, edit and assign Delivery Result Servers to Deliveries."};
