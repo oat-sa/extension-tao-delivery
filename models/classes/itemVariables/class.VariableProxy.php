b832002f2d94d128b1e0dfbd5f0b5b96f489b98d
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-08-14 09:38:32 +0000

    using Filesystem persistence
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoDelivery/trunk@15377 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/itemVariables/class.VariableProxy.php
+++ models/classes/itemVariables/class.VariableProxy.php
@@ -34,7 +34,7 @@ class taoDelivery_models_classes_itemVariables_VariableProxy
 	private $implementation;
 	
 	protected function __construct() {
-		$this->implementation = new taoDelivery_models_classes_itemVariables_RedisPersistence();
+		$this->implementation = new taoDelivery_models_classes_itemVariables_FileSystemPersistence();
 		parent::__construct();
 	}
 	
