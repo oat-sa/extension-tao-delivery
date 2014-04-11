5eaa14fc405b20be565096faf2bac66588f0ac1d
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-11 14:30:03 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoDelivery/trunk@19657 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -84,8 +84,6 @@ return array(
 	
 	 	#TAO extension Paths
 		'TAOBASE_WWW'			=> ROOT_URL  . 'tao/views/',
-		'TAOVIEW_PATH'			=> $taopath.'views'.DIRECTORY_SEPARATOR,
-		'TAO_TPL_PATH'			=> $taopath.'views'.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR,
 	
 		#PROCESS BASE WWW the web path of the process authoring tool
 		'PROCESS_BASE_WWW'		=> ROOT_URL. 'wfEngine/views/',
@@ -99,4 +97,3 @@ return array(
 
 	)
 );
-?>
