aa263f4a44bf51dcc67974d3c98cbd49c25ff892
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-05 15:19:04 +0000

    changed description (ran taoTranslate.php -v -a compile -l en-US -e taoDelivery and update )
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoDelivery/trunk@20064 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/en-US/messages.lang.php
+++ locales/en-US/messages.lang.php
@@ -1,3 +1,4 @@
 <?php
+$GLOBALS['__l10n']["The Deliveries module allows users to define tests deliveries parameters."]="Schedule test deliveries by defining the group (who), the test (what), and the date/time (when).  You can add more detailed specifications such as if test-takers are allowed to retake a test and where to send and store the results data.";
 
 ?>
--- locales/en-US/messages_po.js
+++ locales/en-US/messages_po.js
@@ -3,6 +3,6 @@
 define(function(){
  return {
    langCode : 'en-US',
-   i18n_tr : []
+   i18n_tr : {"The Deliveries module allows users to define tests deliveries parameters.":"Schedule test deliveries by defining the group (who), the test (what), and the date\/time (when).  You can add more detailed specifications such as if test-takers are allowed to retake a test and where to send and store the results data."}
  };
 });
