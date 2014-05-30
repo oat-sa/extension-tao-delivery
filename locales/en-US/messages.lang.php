48377bf5b430e6a2b1be910c084eb4bd8bb5e909
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-30 15:49:55 +0000

    updated and compiled translations
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoDelivery/trunk@20638 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/en-US/messages.lang.php
+++ locales/en-US/messages.lang.php
@@ -4,7 +4,6 @@ $GLOBALS['__l10n']["Group test takers according to global features and classific
 $GLOBALS['__l10n']["Manage test takers"]="Manage test-takers";
 $GLOBALS['__l10n']["Record and manage test takers."]="Record and manage test-takers.";
 $GLOBALS['__l10n']["Test Taker"]="Test-Taker";
-$GLOBALS['__l10n']["Test takers"]="Test-takers";
 $GLOBALS['__l10n']["The Deliveries module allows users to define tests deliveries parameters."]="Schedule test deliveries by defining the group (who), the test (what), and the date/time (when).  You can add more detailed specifications such as if test-takers are allowed to retake a test and where to send and store the results data.";
 $GLOBALS['__l10n']["To be accessible by test takers on the specified dates, you have to publish your delivery."]="To be accessible by test-takers on the specified dates, you have to publish your delivery.";
 
