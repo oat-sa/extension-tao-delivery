f25e0d80a987a72f9e33edcdb89463261850e5f0
Lionel Lecaque <lionel@taotesting.com>
2015-06-18 17:15:44 +0200

    increase version number +build js + css lg

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return array(
 	'label' => 'Delivery core extension',
 	'description' => 'TAO delivery extension manges the administration of the tests',
     'license' => 'GPL-2.0',
-    'version' => '2.6.2',
+    'version' => '2.6.3',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'taoTests' => '>=2.6',
