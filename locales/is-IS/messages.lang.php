358697da76202e3aea8b707bd97f7020121d693f
llecaque <lionel@taotesting.com>
2015-08-11 14:08:59 +0200

    translation from pootle

--- locales/is-IS/messages.lang.php
+++ locales/is-IS/messages.lang.php
@@ -1,4 +1,5 @@
 <?php
+$GLOBALS['__l10n']["\"list saved\""]="\"listi vistaður\"";
 $GLOBALS['__l10n']["%d day"]="%d dagur";
 $GLOBALS['__l10n']["%d days"]="%d dagar";
 $GLOBALS['__l10n']["%h hour"]="%h klukkutími";
