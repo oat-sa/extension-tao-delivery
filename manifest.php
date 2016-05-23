758723df3e0dec3ae23add4117bd2bc547399918
jsconan <jsconan@gmail.com>
2016-05-23 11:29:01 +0200

    Update version to 3.4.0

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return array(
 	'label' => 'Delivery core extension',
 	'description' => 'TAO delivery extension manges the administration of the tests',
     'license' => 'GPL-2.0',
-    'version' => '3.3.2',
+    'version' => '3.4.0',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'tao' => '>=2.17.0',
