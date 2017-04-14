03896eeb805181742644ff5ffdc80a8bb6ed230f
Joel Bout <joel@taotesting.com>
2017-04-14 17:37:25 +0200

    Version bump

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return array(
     'label' => 'Delivery core extension',
     'description' => 'TAO delivery extension manges the administration of the tests',
     'license' => 'GPL-2.0',
-    'version' => '5.0.1',
+    'version' => '5.0.2',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'tao' => '>=7.31.0',
