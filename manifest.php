80d90cb23c884e6f87152af186bea2c3047efdca
Alexander Zagovorichev <zagovorichev@gmail.com>
2017-06-09 15:40:37 +0300

    fixed manifest version

--- manifest.php
+++ manifest.php
@@ -30,7 +30,7 @@ return array(
     'label' => 'Delivery core extension',
     'description' => 'TAO delivery extension manges the administration of the tests',
     'license' => 'GPL-2.0',
-    'version' => '6.1.1',
+    'version' => '6.1.2',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'tao' => '>=10.18.0',
