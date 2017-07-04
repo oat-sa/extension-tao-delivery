62a06c4223acb6236eee865a6f7ebf67bbaf7a7d
Aleksej Tikhanovich <aleksej@taotesting.com>
2017-07-04 13:52:16 +0300

    Added custom label for deliveries

--- manifest.php
+++ manifest.php
@@ -41,6 +41,7 @@ return array(
         'php' => array(
             __DIR__.DIRECTORY_SEPARATOR.'scripts'.DIRECTORY_SEPARATOR.'install'.DIRECTORY_SEPARATOR.'registerEntryPoint.php',
              \oat\taoDelivery\scripts\install\installDeliveryLogout::class,
+             \oat\taoDelivery\scripts\install\installDeliveryFields::class,
         )
     ),
     'update' => 'oat\\taoDelivery\\scripts\\update\\Updater',
