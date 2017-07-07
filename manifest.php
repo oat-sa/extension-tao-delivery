a49297e7816123d2388d3c277471d3374ce9c0e6
Sam Sipasseuth <sam@taotesting.com>
2017-07-07 09:48:56 +0200

    bump version

--- manifest.php
+++ manifest.php
@@ -31,7 +31,7 @@ return array(
     'label' => 'Delivery core extension',
     'description' => 'TAO delivery extension manges the administration of the tests',
     'license' => 'GPL-2.0',
-    'version' => '6.3.0',
+    'version' => '6.3.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'tao' => '>=10.20.0',
