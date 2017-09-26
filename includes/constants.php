7d47ad9f7f47f6e75da6f274891b5197f22761c6
Ionut Padurariu <ionut@taotesting.com>
2017-09-26 09:26:10 +0200

    feature/TAO-5179/omit-expired-deliveries - not yet deprecated

--- includes/constants.php
+++ includes/constants.php
@@ -25,9 +25,7 @@ $todefine = array(
     
     'PROPERTY_DELVIERYEXECUTION_DELIVERY'  => 'http://www.tao.lu/Ontologies/TAODelivery.rdf#DeliveryExecutionDelivery',
     'PROPERTY_DELVIERYEXECUTION_SUBJECT'   => 'http://www.tao.lu/Ontologies/TAODelivery.rdf#DeliveryExecutionSubject',
-    // @deprecated
 	'PROPERTY_DELVIERYEXECUTION_START'     => 'http://www.tao.lu/Ontologies/TAODelivery.rdf#DeliveryExecutionStart',
-    // @deprecated
 	'PROPERTY_DELVIERYEXECUTION_END'       => 'http://www.tao.lu/Ontologies/TAODelivery.rdf#DeliveryExecutionEnd',
     'PROPERTY_DELVIERYEXECUTION_STATUS'    => 'http://www.tao.lu/Ontologies/TAODelivery.rdf#StatusOfDeliveryExecution',
 );
