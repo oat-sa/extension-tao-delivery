852b89309d44fed99f758f4935d06b367bd78421
Ionut Padurariu <ionut.padurariu90@gmail.com>
2017-12-29 10:51:32 +0100

    deprecate constants

--- model/DeliveryContainerService.php
+++ model/DeliveryContainerService.php
@@ -28,9 +28,11 @@ use oat\taoDelivery\model\execution\DeliveryExecution;
  */
 interface  DeliveryContainerService  {
 
-	const PROPERTY_PERIOD_START = 'http://www.tao.lu/Ontologies/TAODelivery.rdf#PeriodStart';
+    /** @deprecated use DeliveryAssemblyService::PROPERTY_PERIOD_START */
+    const PROPERTY_PERIOD_START = 'http://www.tao.lu/Ontologies/TAODelivery.rdf#PeriodStart';
 
-	const PROPERTY_PERIOD_END = 'http://www.tao.lu/Ontologies/TAODelivery.rdf#PeriodEnd';
+    /** @deprecated use DeliveryAssemblyService::PROPERTY_PERIOD_END */
+    const PROPERTY_PERIOD_END = 'http://www.tao.lu/Ontologies/TAODelivery.rdf#PeriodEnd';
 
     /** @deprecated */
     const CONFIG_ID = 'taoDelivery/deliveryContainer';
