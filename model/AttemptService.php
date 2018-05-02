f2547c2590f9572a58d84b041bcab139178366b8
Aleh Hutnikau <hutnikau@1pt.com>
2018-05-02 14:57:45 +0300

    Add AttemptService interface

---
+++ model/AttemptService.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ */
+
+namespace oat\taoDelivery\model;
+
+use oat\oatbox\user\User;
+use oat\taoDelivery\model\execution\DeliveryExecutionInterface;
+
+/**
+ * Service to count the attempts to pass the test.
+ *
+ * @access public
+ * @author Aleh Hutnikau
+ * @package taoDelivery
+ */
+interface AttemptService
+{
+    const SERVICE_ID = 'taoDelivery/AttemptService';
+
+    /**
+     * Get array of delivery finished delivery executions which should be considered as an attempt.
+     *
+     * @param \core_kernel_classes_Resource $delivery
+     * @param User $user
+     * @return DeliveryExecutionInterface[]
+     */
+    public function getAttempts(\core_kernel_classes_Resource $delivery, User $user);
+}
