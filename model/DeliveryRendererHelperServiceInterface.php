08e4e0f0f6627457f789c98d1df29b7b94ba4644
Vilmos "Chilly" Kovacs <vilmos@taotesting.com>
2018-10-26 10:03:18 +0200

    update licence

--- model/DeliveryRendererHelperServiceInterface.php
+++ model/DeliveryRendererHelperServiceInterface.php
@@ -14,8 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2018 (original work) Open Assessment Technologies SA;
- *
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  */
 
 namespace oat\taoDelivery\model;
