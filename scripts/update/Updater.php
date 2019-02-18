c2753a5f4613e02d44e697338b8d85d4f07bd818
Aleh Hutnikau <hutnikau@1pt.com>
2019-02-18 18:12:11 +0300

    bump versoin

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -397,6 +397,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('12.1.0');
         }
 
-        $this->skip('12.1.0', '12.4.0');
+        $this->skip('12.1.0', '12.4.1');
     }
 }
