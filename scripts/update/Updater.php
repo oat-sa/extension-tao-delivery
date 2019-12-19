0d7e24947d7ad00a3f3a0bf10622881aae174014
Augustas Nedzinskas <augustas@taotesting.com>
2019-12-19 23:45:24 +0100

    version bump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -445,6 +445,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('14.3.0');
         }
 
-        $this->skip('14.3.0', '14.6.0');
+        $this->skip('14.3.0', '14.7.0');
     }
 }
