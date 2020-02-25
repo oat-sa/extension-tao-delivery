064e22f10b1f3cc81deb8945773b676c15c6a73d
lecosson <lecosson@gmail.com>
2020-02-25 15:56:09 +0300

    ACTP-313 Delivery/Proctoring Page - Fix HTML Structure reverted

--- config/default/execution_service.conf.php
+++ config/default/execution_service.conf.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The default Tao DeliveryExecution tracking service
- *
+ * 
  * It uses the ontology to store the deliveryExecutions of the students
  */
 return new \oat\taoDelivery\model\execution\OntologyService();
