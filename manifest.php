756acd23bdfe2b54b65a311e774683802eac4425
Arthur Paveliev <arthur.paveliev@taotesting.com>
2020-03-27 13:37:00 +0300

    Updated version

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return [
     'label' => 'Delivery core extension',
     'description' => 'TAO delivery extension manges the administration of the tests',
     'license' => 'GPL-2.0',
-    'version' => '14.9.1',
+    'version' => '14.9.2',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'tao' => '>=41.9.1',
