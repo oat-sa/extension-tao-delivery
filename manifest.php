f6263c05f3860fd33dfc78ee9270dce9ea111061
zagovorichev <zagovorichev@gmail.com>
2020-05-13 17:23:27 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return [
     'label' => 'Delivery core extension',
     'description' => 'TAO delivery extension manges the administration of the tests',
     'license' => 'GPL-2.0',
-    'version' => '14.13.1',
+    'version' => '14.14.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => [
         'tao' => '>=41.10.0',
