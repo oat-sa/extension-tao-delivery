97093da4df904229216ec7960912a9b18480115f
Andrew Shapiro <shpr.andrew@gmail.com>
2020-10-14 10:21:01 +0300

    Feature/BSA-272/Fix migration

--- migrations/Version202010131343168613_taoDelivery.php
+++ migrations/Version202010131343168613_taoDelivery.php
@@ -1,20 +1,35 @@
 <?php
 
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ */
+
 declare(strict_types=1);
 
 namespace oat\taoDelivery\migrations;
 
 use Doctrine\DBAL\Schema\Schema;
-use common_exception_Error as Error;
-use common_ext_Extension as Extension;
-use common_ext_ExtensionsManager as ExtensionsManager;
 use oat\tao\scripts\tools\migrations\AbstractMigration;
-use common_ext_ExtensionException as ExtensionException;
-use oat\oatbox\service\exception\InvalidServiceManagerException;
 use oat\taoDelivery\scripts\tools\ConfigureDeliveryExecutionHeader;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Class Version202010131343168613_taoDelivery
+ *
+ * @package oat\taoDelivery\migrations
  */
 final class Version202010131343168613_taoDelivery extends AbstractMigration
 {
@@ -33,7 +48,7 @@ final class Version202010131343168613_taoDelivery extends AbstractMigration
      */
     public function up(Schema $schema): void
     {
-        $this->propagate(new ConfigureDeliveryExecutionHeader())();
+        $this->propagate(new ConfigureDeliveryExecutionHeader())([]);
     }
 
     /**
