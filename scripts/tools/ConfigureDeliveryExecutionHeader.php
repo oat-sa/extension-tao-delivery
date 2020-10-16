d1323dde85ebac63ad196c67c7e2cd8a54fc847a
Andrew Shapiro <59471572+shpran@users.noreply.github.com>
2020-10-16 11:47:34 +0300

    Update scripts/tools/ConfigureDeliveryExecutionHeader.php

--- scripts/tools/ConfigureDeliveryExecutionHeader.php
+++ scripts/tools/ConfigureDeliveryExecutionHeader.php
@@ -29,7 +29,6 @@ use common_ext_ExtensionsManager as ExtensionsManager;
 use common_ext_ExtensionException as ExtensionException;
 use oat\oatbox\service\exception\InvalidServiceManagerException;
 
-/**
 class ConfigureDeliveryExecutionHeader extends ScriptAction
 {
     public const OPTION_HIDE_HOME_BUTTON = 'hideHomeButton';
