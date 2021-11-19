5fb04efcf9017f24bad63e4ad4a2499dc0056824
Eugene Golubev <emgolubev@gmail.com>
2021-11-19 15:13:57 +0300

    fix: CR fix

--- config/default/deliveryServer.conf.php
+++ config/default/deliveryServer.conf.php
@@ -1,8 +1,6 @@
 <?php
 
 use oat\taoDelivery\model\execution\DeliveryServerService;
-use oat\taoDelivery\model\execution\implementation\ResultServerServiceFactory;
 
 return new DeliveryServerService([
-    DeliveryServerService::OPTION_RESULT_SERVER_SERVICE_FACTORY => new ResultServerServiceFactory()
 ]);
